e7b63cd13a62d4d7efcdaf2a30eb05a99bcef383
rajansurya <rajansurya.88@gmail.com>
2025-05-03 23:53:11 +0530

    responsive

--- lib/widgets/Expenses.dart
+++ lib/widgets/Expenses.dart
@@ -5,7 +5,6 @@ import 'chart/chart.dart';
 import 'expenses_list/expenses_list.dart';
 import 'new_expenses.dart';
 
-
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
@@ -33,6 +32,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(onAddExpense: _addExpense),
@@ -69,6 +69,7 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     Widget mainContent = const Center(
       child: Text('No expenses found. Start adding some!'),
     );
@@ -90,14 +91,22 @@ class _ExpensesState extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(
-            child: mainContent,
-          ),
-        ],
-      ),
+      body: width < 500
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(
+                  child: mainContent,
+                ),
+              ],
+            )
+          : Row(children: [
+             Expanded(child:  Chart(expenses: _registeredExpenses),flex: 45,),
+              Expanded(
+                flex: 55,
+                child: mainContent,
+              ),
+            ]),
     );
   }
 }
