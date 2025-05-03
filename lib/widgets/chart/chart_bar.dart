171e159913fe3f71bfea8e7f5cca7e4d21b9f438
rajansurya <rajansurya.88@gmail.com>
2025-05-03 19:49:19 +0530

    space

--- lib/widgets/chart/chart_bar.dart
+++ lib/widgets/chart/chart_bar.dart
@@ -10,6 +10,7 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     final isDarkMode =
         MediaQuery.of(context).platformBrightness == Brightness.dark;
     return Expanded(
